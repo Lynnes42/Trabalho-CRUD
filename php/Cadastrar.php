30520652c175cbf5890fc84419003ff0ba63cb13
Nick <m.evelynreis@gmail.com>
2025-07-06 18:13:00 -0300

    Update Cadastrar.php

--- php/Cadastrar.php
+++ php/Cadastrar.php
@@ -1,5 +1,11 @@
 <?php require_once 'banco.php'; ?>
-
+<?php
+    session_start();
+        if(!isset($_SESSION['logado']) || $_SESSION['logado'] !== true){
+            header("Location: ../index.html");
+            exit;
+        }
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
